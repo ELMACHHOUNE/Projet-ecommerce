3e07270c5d63475caa73423cf7780b7dfa00df8a
MOHAMED EL MACHHOUNE <mohamed20rida@gmail.com>
2023-12-20 16:38:46 +0100

    add

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function addToCart(product, price) {
     const quantity = parseInt(prompt(`Enter quantity for ${product}:`) || 0);
     if (quantity > 0) {
         const total = price * quantity;
-        cart.push({ product, price, quantity, total });
+        const cartItem = { product, price, quantity, total };
+        cart.push(cartItem);
         updateCart();
     } else {
         alert('Invalid quantity. Please enter a valid number.');
@@ -13,9 +14,41 @@ function addToCart(product, price) {
 }
 
 function updateCart() {
+    const cartItemsDiv = document.getElementById('cart-items');
+    const totalPriceSpan = document.getElementById('total-price');
+    cartItemsDiv.innerHTML = '';
+
     let totalPrice = 0;
     for (const item of cart) {
+        const itemDiv = document.createElement('div');
+        itemDiv.className = 'cart-item';
+        itemDiv.innerHTML = `
+            <p>${item.product}</p>
+            <p>Price: $${item.price}</p>
+            <p>Quantity: ${item.quantity}</p>
+            <p>Total: $${item.total}</p>
+            <button onclick="deleteProduct('${item.product}')">Delete</button>
+        `;
+        cartItemsDiv.appendChild(itemDiv);
         totalPrice += item.total;
     }
-    alert(`Cart Total: $${totalPrice}`);
+
+    totalPriceSpan.textContent = `$${totalPrice.toFixed(2)}`;
+}
+
+function deleteProduct(product) {
+    const index = cart.findIndex(item => item.product === product);
+    if (index !== -1) {
+        cart.splice(index, 1);
+        updateCart();
+    }
+}
+
+function buyProducts() {
+    // Implement your buy logic here, for example, redirect to a checkout page.
+    alert('Redirecting to checkout...');
+}
+
+function scrollToCategories() {
+    document.getElementById('categories').scrollIntoView({ behavior: 'smooth' });
 }
